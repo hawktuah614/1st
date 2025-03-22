8988160f95c24afa8776b45e2d90e9b54fd32bac
hawktuah614 <goofysheesh@gmail.com>
2025-03-22 17:37:52 +0800

    new scenes
    i added level 1 and 2 , 3 will be coming soon

---
+++ scene4/main_menu.gd
@@ -0,0 +1,9 @@
+extends Node
+
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene4/scene3/level1_2d.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene4/scene3/level2_2d.tscn")
